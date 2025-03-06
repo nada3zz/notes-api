8ec4d0e4bf5de8485cca52870e448a2d9840b4ec
nadaeezzdin <nadaeezzdin@gmail.com>
2025-03-06 17:24:34 +0200

    increase access time

--- src/app/shared/config/auth.config.ts
+++ src/app/shared/config/auth.config.ts
@@ -6,8 +6,9 @@ export default registerAs(
     jwt: {
       accessToken: {
         secretKey: process.env.AUTH_JWT_ACCESS_TOKEN_SECRET_KEY,
-        expirationTime: 30,
+        expirationTime: 1000,
       },
     },
   }),
 );
+
