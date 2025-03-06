21ac17ab6125f915605d3b7429d1d26c301ccedb
nadaeezzdin <nadaeezzdin@gmail.com>
2025-03-06 04:43:55 +0200

    add global validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes( new ValidationPipe({
+    transform: true,
+    whitelist: true
+  }));
+  const port = configService.get<number>('PORT') || 3000;
+  await app.listen(port);
 }
 bootstrap();
